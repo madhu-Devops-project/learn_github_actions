f19a8fd880d052aec8e9a69cd0879c9f63a3c741
Live session user <madhuvgn003@gmail.com>
2026-07-06 21:21:36 +0530

    add initial flask script

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
-printf("Hello")
+from flask import Flask
 
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return {
+        "message": "Welcome to DevOps CI/CD Project!"
+    }
+
+
+@app.route("/health")
+def health():
+    return {
+        "status": "UP"
+    }
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
